c3ade88ad431f393e320ea1946cca39fa40eebdc
Alex <av.lobachev@gmail.com>
2019-03-25 09:07:50 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from var_dump import var_dump
+from pprint import pprint
+
+
+class Converter:
+    def __init__(self, file_name, FileReader):
+        self.file_name = file_name
+        self.FileReader = FileReader
+
+    def load_file(self):
+        return FileReader.read(self, self.file_name)
+
+    def save_file(self, FileWriter):
+        FileWriter.write(self.file_name)
+
+
+class FileReader:
+    def read(self, file_name):
+        with open(file_name, 'r') as f:
+            return list(f.readlines())
+
+
+class FileWriter:
+    def save(self, file_name, lines):
+        with open(file_name, 'w') as f:
+            f.writelines(self, lines)
+
+
+def main():
+    file = 'D:\YandexDisk\Python\PythonProjects\SublimePlugin\import_small.csv'
+    file_rider = FileReader()
+    converter = Converter(file, file_rider)
+    print(converter.load_file())
+
+
+if __name__ == '__main__':
+    main()
